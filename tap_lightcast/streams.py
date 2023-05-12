f71b5cbbb5ff6ac11b03e1efb4fb266365625549
Giovanni Bortolussi <gbortolussi@degreed.com>
2023-05-12 10:52:21 +0200

    latest changes

--- tap_lightcast/streams.py
+++ tap_lightcast/streams.py
@@ -4,6 +4,7 @@ import logging
 import requests
 from typing import Dict, Optional, Any
 from singer_sdk import typing as th
+from singer_sdk import Tap
 from singer_sdk.streams import RESTStream
 from singer_sdk.authenticators import APIAuthenticatorBase, OAuthAuthenticator
 
@@ -29,6 +30,41 @@ class LightcastOAuthAuthenticator(OAuthAuthenticator):
 class TapLightcastStream(RESTStream):
     """Generic Lightcast stream class."""
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        logging.info("Parse Lightcast latest version")
+        # Create new token
+        data = {
+            "client_id": self.config["client_id"],
+            "client_secret": self.config["client_secret"],
+            "grant_type": "client_credentials",
+            "scope": "emsi_open",
+        }
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        response = requests.request(
+            "POST", url=auth_endpoint, data=data, headers=headers
+        )
+        access_token = response.json()["access_token"]
+        # Parse latestVersion
+        headers = {"Authorization": "Bearer {}".format(access_token)}
+        url = url_base + "/meta"
+        response = requests.request("GET", url=url, headers=headers)
+        self.latestVersion = response.json()["data"]["latestVersion"]
+        if "replication_key_value" in self.stream_state:
+            logging.warn(
+                "################## replication_key_value already in stream_state"
+            )
+            self.replication_key = self.stream_state["replication_key"]
+            self.replication_key_value = self.stream_state["replication_key_value"]
+        else:
+            logging.warn("################## no replication_key_value in stream_state")
+            self.stream_state["replication_key"] = "latestVersion"
+            self.stream_state["replication_key_value"] = self.latestVersion
+            self.replication_key = "latestVersion"
+            self.replication_key_value = ""
+            logging.warn(self.stream_state)
+
     @property
     def url_base(self) -> str:
         """Base URL of source"""
@@ -41,60 +77,43 @@ class TapLightcastStream(RESTStream):
             auth_endpoint=auth_endpoint,
         )
 
+
+class SkillsList(TapLightcastStream):
+    def __init__(self, tap: Tap):
+        super().__init__(tap)
+        self.logger = logging.getLogger(__name__)
+
+    name = "skills_list"  # Stream name
+    primary_keys = ["id"]
+    records_jsonpath = "$.data[0:]"  # https://jsonpath.com Use requests response json to identify the json path
+    path = ""
+    replication_key = "latestVersion"
+
+    schema = th.PropertiesList(
+        th.Property("id", th.StringType), th.Property("latestVersion", th.StringType)
+    ).to_dict()
+
     def prepare_request(
         self, context: Optional[dict] | None, next_page_token: Optional[Any] | None
     ) -> requests.PreparedRequest:
-
-        if self.latestVersion and self.latestVersion == "":
-            logging.warn("################### PARSING latestVersion")
-            client_id = self.config["client_id"]
-            client_secret = self.config["client_secret"]
-            data = {
-                "client_id": client_id,
-                "client_secret": client_secret,
-                "grant_type": "client_credentials",
-                "scope": "emsi_open",
-            }
-            headers = {"Content-Type": "application/x-www-form-urlencoded"}
-            response = requests.request(
-                "POST", url=auth_endpoint, data=data, headers=headers
-            )
-            access_token = response.json()["access_token"]
-            headers = {"Authorization": "Bearer {}".format(access_token)}
-            url = url_base + "/meta"
-            response = requests.request("GET", url=url, headers=headers)
-            latestVersion = response.json()["data"]["latestVersion"]
-            logging.warn("################### latestVersion: " + latestVersion)
 
         http_method = self.rest_method
         url: str = "{url_base}/versions/{latestVersion}/skills".format(
             url_base=url_base, latestVersion=self.latestVersion
         )
-        logging.warning("################### url:" + url)
         params = {"fields": "id"}
         if "limit" in self.config:
             params.update({"limit": self.config["limit"]})
-        # if context["stopTap"] == True:
-        #     params.update({"q": ""})
-        # request_data = self.prepare_request_payload(context, next_page_token)
+        # if "replication_key_value" in self.stream_state:
+        #     if self.stream_state["replication_key_value"] == self.latestVersion:
+        #         params.update({"q": ""})
         headers = self.http_headers
-
         return self.build_prepared_request(
             method=http_method,
             url=url,
             params=params,
             headers=headers,
-            # json=request_data,
         )
-
-
-class SkillsList(TapLightcastStream):
-    name = "skills_list"  # Stream name
-    primary_keys = ["id"]
-    records_jsonpath = "$.data[0:]"  # https://jsonpath.com Use requests response json to identify the json path
-    path = ""
-
-    schema = th.PropertiesList(th.Property("id", th.StringType)).to_dict()
 
     # https://sdk.meltano.com/en/latest/parent_streams.html
     def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
@@ -103,14 +122,15 @@ class SkillsList(TapLightcastStream):
 
 
 class SkillsDetails(TapLightcastStream):
-
-    name = "skills_details"  # Stream name
     parent_stream_type = SkillsList
-    path = "/{id}"  # API endpoint after base_url
+    name = "skills_details"  # Stream name
     primary_keys = ["id"]
     records_jsonpath = "$.data[0:]"  # https://jsonpath.com Use requests response json to identify the json path
+    path = "/versions/{latestVersion}/skills/{id}"  # API endpoint after base_url
+    replication_key = "latestVersion"
 
     schema = th.PropertiesList(
+        th.Property("latestVersion", th.StringType),
         th.Property("id", th.StringType),
         th.Property("name", th.StringType),
         th.Property("type", th.ObjectType(th.Property("id", th.StringType))),
